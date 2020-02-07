bd910020b1ae687522e0c15c155f71c2899d7a04
jimcturner <james@jamescturner.co.uk>
2020-02-07 14:11:21 +0000

    Windows test....

--- __main__.py
+++ __main__.py
@@ -3419,7 +3419,7 @@ class ResultsTransmitter(object):
                     # iterate over fragments
                     for fragment in fragmentedMessage:
                         # Pickle and send each fragment one at a time
-                        txMessage = pickle.dumps(fragment)
+                        txMessage = pickle.dumps(fragment).encode('ascii')
                         # Message.addMessage("tx'd: (" +str(len(txMessage)) + ") "+ txMessage)
                         self.udpSocket.sendto(txMessage, (self.destAddr, self.destPort))
 
