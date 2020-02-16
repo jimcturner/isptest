8a62563ac28aca5b7ae9b5a6445e075a042ccfdb
jimcturner <james@jamescturner.co.uk>
2020-02-16 23:40:09 +0000

    Windows test....

--- __main__.py
+++ __main__.py
@@ -1640,7 +1640,7 @@ def myUnpickler(input):
     # Try Python3 version first (has the 'fix_imports' keyword
     try:
         # unPickledMessage = pickle.loads(input, fix_imports=True, encoding="bytes")
-        unPickledMessage = pickle.loads(input, encoding="bytes")
+        unPickledMessage = pickle.loads(input)
         Message.addMessage("U1")
     except Exception as e:
         # Try the Python2 version
