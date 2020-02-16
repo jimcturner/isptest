4c58684ff66eb636877e78a5b28dcc76ebf49f9e
jimcturner <james@jamescturner.co.uk>
2020-02-16 23:00:11 +0000

    Windows test....

--- __main__.py
+++ __main__.py
@@ -1626,7 +1626,7 @@ def myPickler(input):
         # By default Python3 uses 'protocol 3' and unicode strings
         # Force it to use the older (Python2 compatible) protocal 2, and byte (ascii) strings
         # pickledMessage = pickle.dumps(input, protocol=2, encoding='bytes')
-        pickledMessage = pickle.dumps(input, protocol=2, encoding='latin1')
+        pickledMessage = pickle.dumps(input, protocol=2, encoding='latin-1')
     except:
         Message.addMessage("P2")
         # If that fails, try Python 2's version
