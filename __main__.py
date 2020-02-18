09598e341bb045a837baae37d348f17dfde8fd91
jimcturner <james@jamescturner.co.uk>
2020-02-18 15:42:35 +0000

    Windows test....

--- __main__.py
+++ __main__.py
@@ -4428,6 +4428,14 @@ def main(argv):
             except Exception as e:
                 Message.addMessage(Term.FG(Term.RED) + "__main()sock.recvfrom(): Cannot read socket " + UDP_RX_IP + ":" + \
                     str(UDP_RX_PORT) + ", " + str(e))
+                Message.addMessage("Attempting to recreate receive socket binding on " + UDP_RX_IP + ":" + str(UDP_RX_PORT))
+                try:
+                    sock = socket.socket(socket.AF_INET,  # Internet
+                                         socket.SOCK_DGRAM)  # UDP
+                    sock.bind((UDP_RX_IP, UDP_RX_PORT))
+                except Exception as e:
+                    Message.addMessage(
+                        Term.FG(Term.RED) + "__main(): Cannot create socket listen on " + UDP_RX_IP + ":" + str(UDP_RX_PORT) + ", " + str(e))
                 time.sleep(2)
                 # exit()
 
