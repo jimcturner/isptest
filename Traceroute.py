a9c2153fbbf8b4ff57d44bbffa1b173705ca98d6
jimcturner <james@jamescturner.co.uk>
2020-04-24 16:36:59 +0100

    rudimentary resolves addr to hostname

--- Traceroute.py
+++ Traceroute.py
@@ -2,23 +2,34 @@
 
 from scapy.all import *
 from scapy.layers.inet import IP, UDP
+
+import socket
 def traceroute():
-    pass
     hostname = "google.com"
     for i in range(1, 28):
         # print("sending with TTL " + str(i))
         pkt = IP(dst=hostname, ttl=i) / UDP(dport=33434)
         # Send the packet and get a reply
-        reply = sr1(pkt, verbose=0, timeout=5, iface="en16")
+        reply = sr1(pkt, verbose=0, timeout=1)
         if reply is None:
             print(str(i) + " *")
             # break
         elif reply.type == 3:
             # We've reached our destination
-            print ("Done! "+ str(reply.src))
-
+            resolvedName = ""
+            try:
+                resolvedName = str(socket.gethostbyaddr(str(reply.src)))
+            except:
+                resolvedName =""
+            print ("Done! "+ str(reply.src)+ ",  " + resolvedName)
             break
         else:
             # We're in the middle somewhere
-            print (str(i) + " "+ str(reply.src))
+            resolvedName = ""
+            try:
+                resolvedName = str(socket.gethostbyaddr(str(reply.src)))
+            except:
+                resolvedName =""
+
+            print (str(i) + " "+ str(reply.src) + ",  " + resolvedName)
 
