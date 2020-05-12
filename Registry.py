c1554e10446dc91dcb3f47c4991b306dc138408a
jimcturner <james@jamescturner.co.uk>
2020-05-12 17:11:40 +0100

    changes filename of log file from log.txt to isptest_log.txt

--- Registry.py
+++ Registry.py
@@ -15,7 +15,7 @@ class Registry(object):
     tracerouteMaxHops = 28 # The maximum no of hops traceroute will consider before resetting
     tracerouteFallbackUDPDestPort = 33434 # The 'fallback' port used by the RtpGenerator.traceroute thread if no reply
                                             # is received from a host
-    messageLogFilename = "log.txt"  # This file is appended to, every time Utils.Message.addMessage() is called
+    messageLogFilename = "isptest_log.txt"  # This file is appended to, every time Utils.Message.addMessage() is called
     # Provides content for the help popup
     helpTableContents = [["h","Display/hide this page"],
                          ["Ctrl-C", "Quit the application"],
