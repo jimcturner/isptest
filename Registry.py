0fcdf973de0202700caadbcf6360670f806660a1
jimcturner <james@jamescturner.co.uk>
2020-05-13 15:42:46 +0100

    Commit changes to remote

--- Registry.py
+++ Registry.py
@@ -4,7 +4,7 @@
 # This will be used as the source of default values
 class Registry(object):
     streamReportFilename = "Stream_report_"
-    version = "1.7"
+    version = "1.8"
     pythonMinimumVersionRequired_Major = 3  # Specfify the minimum version of the Python interpreter required
     pythonMinimumVersionRequired_Minor = 6  # This equates to Python version 3.6
 
@@ -12,9 +12,7 @@ class Registry(object):
     receiverLogFilename = "isptest_Receiver_Events_Log"
     transmitterLogFilename= "isptest_Transmitter_Events_Log"
     maximumLogFileSize_bytes = 1024 * 1024 # 1Mb maximum size
-    tracerouteMaxHops = 28 # The maximum no of hops traceroute will consider before resetting
-    tracerouteFallbackUDPDestPort = 33434 # The 'fallback' port used by the RtpGenerator.traceroute thread if no reply
-                                            # is received from a host
+
     messageLogFilename = "isptest_log.txt"  # This file is appended to, every time Utils.Message.addMessage() is called
     # Provides content for the help popup
     helpTableContents = [["h","Display/hide this page"],
@@ -25,8 +23,14 @@ class Registry(object):
                          ["t", "Show traceroute for the currently selected stream"],
                          ["a", "Show 'About' dialogue"]
                          ]
-    # Getting false positives at the moment (because the CPU can keep up!) so creating lots of unhelpful events
-    allowProcessorOverloadEventGeneration = False
+
     ######### RtpReceiveStream
     lossOfStreamAlarmThreshold_s = 5 # Specifies how long before a loss of stream Event is triggered by RtpReceiveStream
     streamIsDeadThreshold_s = 30 # Specifies how long to wait with no incoming rtp packets before a stream is presumed dead
+    # Getting false positives at the moment (because the CPU can keep up!) so creating lots of unhelpful events
+    allowProcessorOverloadEventGeneration = False
+
+    # RtpGenerator
+    tracerouteMaxHops = 28  # The maximum no of hops traceroute will consider before resetting
+    tracerouteFallbackUDPDestPort = 33434  # The 'fallback' port used by the RtpGenerator.traceroute thread if no reply
+    # is received from a host
