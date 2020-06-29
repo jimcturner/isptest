2acc035d2d9bd41ae67d275bd74a5db5937dd3d9
jimcturner <james@jamescturner.co.uk>
2020-06-29 14:28:45 +0100

    reenables traceroute in Registry

--- Registry.py
+++ Registry.py
@@ -48,7 +48,7 @@ class Registry(object):
 
 
     # RtpGenerator
-    rtpGeneratorEnableTraceroute = False # Enables/inhbits the traceroute thread from starting
+    rtpGeneratorEnableTraceroute = True # Enables/inhbits the traceroute thread from starting
     tracerouteMaxHops = 28  # The maximum no of hops traceroute will consider before resetting
     tracerouteFallbackUDPDestPort = 33434  # The 'fallback' port used by the RtpGenerator.traceroute thread if no reply
     # is received from a host
