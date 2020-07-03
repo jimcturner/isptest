2f824b09d167fef3b4af8af25d15b875109624f5
jimcturner <james@jamescturner.co.uk>
2020-07-03 17:01:06 +0100

    mods rxtest.py

--- rxtest.py
+++ rxtest.py
@@ -81,26 +81,53 @@ def rawReceiveLinux():
             print (str(e))
             pass
 
-        # r, w, x = select.select([rawSocket], [], [])
-        # for i in r:
-        #     receiveSocket = i
-        #     data, addr = receiveSocket.recvfrom(131072)
-        #     print(str(receiveSocket.type) + ", " + str(data))
-        # rawData, rawAddr = rawSocket.recvfrom(131072)
-        # print("raw " + str(rawData))
+def rawReceiveWindows():
+    import select
+    import os
 
-            # # extract IP Header
-            # ipHeader = Utils.IPHeader(data[:20])
-            # udpHeader = Utils.UDPHeader(data[20:28])
-            # icmpMessage = Utils.ICMPHeader(data[20:28])
-            # message = data[28:]
-            # # print(str(i) + ", " + str(i.recvfrom(131072)))
-            # print(str(ipHeader.d_addr) + ":" + ", " + str(ipHeader.protocol) + ", type:" + str(icmpMessage.type) +\
-            #       ", code:" + str(icmpMessage.code))
+    UDP_RX_PORT = 5000
+    UDP_RX_IP = "127.0.0.1"
+    # create UDP socket
+    udpSocket = socket.socket(socket.AF_INET,  # Internet
+                              socket.SOCK_DGRAM)  # UDP
+    udpSocket.settimeout(1)
+    # Create  a raw socket. This *should* get copies of the data received by udpSocket but including the IP header
+    # rawSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    rawSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+    # rawSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+    # rawSocket.settimeout(1)
+    rawSocket.setblocking(0)
+
+    udpSocket.bind((UDP_RX_IP, UDP_RX_PORT))
+    rawSocket.bind((UDP_RX_IP, UDP_RX_PORT))
+    rawSocket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    # rawSocket.ioctl(socket.SIO_RCVALL, socket_RCVALL_ON)
+    print("udpSocket :" + str(udpSocket))
+    print("rawSocket :" + str(rawSocket))
+    while True:
+        try:
+            r, w, x = select.select([udpSocket, rawSocket], [], [])
+            for rxSock in r:
+                data, addr = rxSock.recvfrom(131072)
+                if rxSock == udpSocket:
+                    print(str(rxSock.type) + ", " + str(data))
+                else:
+                    # extract IP Header
+                    ipHeader = IPHeader(data[:20])
+                    udpHeader = UDPHeader(data[20:28])
+                    print (str(ipHeader.d_addr) + ":" + str(udpHeader.destPort) + ", ttl: " + str(ipHeader.ttl) + " " + \
+                           str([data[28:]]))
+        except Exception as e:
+            print (str(e))
+            pass
+
+
+
 
 
 # Main prog starts here
 # #####################
 
 
-rawReceiveLinux()
+# rawReceiveLinux()
+rawReceiveWindows()
