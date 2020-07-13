b327b1c45fc3e6392516f6585fe514fa1fa997f5
jimcturner <james@jamescturner.co.uk>
2020-07-13 18:05:58 +0100

    Commit changes to remote

--- RtpStreams.py
+++ RtpStreams.py
@@ -2127,7 +2127,7 @@ class RtpReceiveStream(RtpReceiveCommon):
                         # Since the rxTTl has changed, we can expect a subsequent change in the received hopslist
                         hopsListChangeExpected = True
                         # Flush the contents of the current hopsList because it's now been invalidated
-                        Utils.Message.addMessage("DBUG: hopsListChangeExpected.")
+                        # Utils.Message.addMessage("DBUG: hopsListChangeExpected.")
                         # self.setTraceRouteHopsList([])
                         # Post a message
                         Utils.Message.addMessage(ipRoutingTTLChange.getSummary(includeStreamSyncSourceID=False)['summary'])
