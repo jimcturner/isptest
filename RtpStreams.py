bccacc20a20e5998cf24357b758c3c5b3ecfde6a
jimcturner <james@jamescturner.co.uk>
2020-07-30 08:12:55 +0100

    Commit changes to remote

--- RtpStreams.py
+++ RtpStreams.py
@@ -5194,7 +5194,7 @@ class RtpGenerator(RtpCommon):
 
         # Determine which Operating System is in use, and therfore which udp tx/icmp rx function we will use
         os = Utils.getOperatingSystem()
-        os = "Windows"
+        # os = "Windows"
         if os == "Windows":
             # Windows detected
             # Create pointer to the correct function for this OS
