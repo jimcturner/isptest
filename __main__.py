7638caa7d122eaefc52c2b8c04e46469d8840024
jimcturner <james@jamescturner.co.uk>
2020-08-08 16:04:31 +0100

    erroneous mod of master branch - added calcIPChecksum() function for testing

--- __main__.py
+++ __main__.py
@@ -3878,27 +3878,50 @@ def shutdownApplicationSignalHandler(signum, frame):
     Utils.Message.addMessage("DBUG: shutdownApplicationSignalHandler() called with signal " + str(signum))
     raise ShutdownApplication
 
-# Takes a list of octets [[a,b,c,d],[a,b,c,d]....] and XORs all contents to a single byte to create a checksum value
-def createTracerouteChecksum(hopsList):
-    if len(hopsList) > 0:
-        try:
-            # Create lambda function to xor two values
-            xor = lambda x, y: x ^ y
-            # Use reduce() to iterate over a the list of octets in sequence using our lambda function
-            xorSingleHop = lambda hopOctets:reduce(xor, hopOctets)
-
-            output = 0
-            # Iterate over the all the hops, xor'ing each hop in turn
-            for hop in hopsList:
-                output = output ^ xorSingleHop(hop)
-            return output
-        except Exception as e:
-            return None
-    else:
-        return None
+# # Takes a list of octets [[a,b,c,d],[a,b,c,d]....] and XORs all contents to a single byte to create a checksum value
+# def createTracerouteChecksum(hopsList):
+#     if len(hopsList) > 0:
+#         try:
+#             # Create lambda function to xor two values
+#             xor = lambda x, y: x ^ y
+#             # Use reduce() to iterate over a the list of octets in sequence using our lambda function
+#             xorSingleHop = lambda hopOctets:reduce(xor, hopOctets)
+#
+#             output = 0
+#             # Iterate over the all the hops, xor'ing each hop in turn
+#             for hop in hopsList:
+#                 output = output ^ xorSingleHop(hop)
+#             return output
+#         except Exception as e:
+#             return None
+#     else:
+#         return None
+# def calcIPChecksum():
+#     import struct
+#
+#     data = "45 00 00 47 73 88 40 00 40 06 a2 c4 83 9f 0e 85 83 9f 0e a1"
+#
+#     def carry_around_add(a, b):
+#         c = a + b
+#         return (c & 0xffff) + (c >> 16)
+#
+#     def checksum(msg):
+#         s = 0
+#         for i in range(0, len(msg), 2):
+#             w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+#             s = carry_around_add(s, w)
+#         return ~s & 0xffff
+#
+#     data = data.split()
+#     data = map(lambda x: int(x,16), data)
+#     data = struct.pack("%dB" % len(data), *data)
+#     print(str(' '.join('%02X' % ord(x) for x in data)))
+#     print("Checksum: " + str(checksum(data)))
 
 
 def main(argv):
+
+
     # print("main thread")
     # textToDisplay = "Say you're writing a program in Python and all it does is pretty print some stuff. The output is in prettiest_print_ever. You already do weird tricks importing fcntl, termios, struct and friends to get the terminal size so that you can use the full width of the terminal (if any); that also gives you the screen height, so it makes sense to use it. (That also means you've long given up any pretenses of cross-platform compatibility, too.)"
     #
