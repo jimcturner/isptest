d9bc334ec6e6d389a3030acd3d6a005650eab2e8
jimcturner <james@jamescturner.co.uk>
2020-09-28 16:09:47 +0100

    changes version to 2.6. Changelog updated

--- Registry.py
+++ Registry.py
@@ -4,7 +4,7 @@
 # This will be used as the source of default values
 class Registry(object):
     streamReportFilename = "Stream_report_"
-    version = "2.5"
+    version = "2.6"
     pythonMinimumVersionRequired_Major = 3  # Specfify the minimum version of the Python interpreter required
     pythonMinimumVersionRequired_Minor = 7  # This equates to Python version 3.7
 
