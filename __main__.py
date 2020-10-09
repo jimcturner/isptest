4d60ef74b676dfbe46ff0eb09d3b06ae9e34f56f
jimcturner <james@jamescturner.co.uk>
2020-10-09 17:07:09 +0100

    Commit changes to remote

--- __main__.py
+++ __main__.py
@@ -2352,7 +2352,7 @@ class UI(object):
                         "\n" + "  and event logging".center(maxWidth, " ") + \
                         "\n\n\n" + "Comments/feedback to: james.c.turner@bbc.co.uk".center(maxWidth, " ") + \
                         "\n See https://confluence.dev.bbc.co.uk/x/ioKKD for support" + \
-                        "\n\n\nlast merge: txHelpPageRetLoss 9/10/20 15:01\n" + \
+                        "\n\n\nlast merge: compressPickles 9/10/20 17:05\n\n" + \
                         "Press the [any] key to continue".center(maxWidth, " ")
 
         # Render the message in a pop-up box
