813a44520843a01f9716a0ac755dfa296bca2400
jimcturner <james@jamescturner.co.uk>
2021-01-22 14:16:36 +0000

    Commit changes to remote

--- RtpStreams.py
+++ RtpStreams.py
@@ -3400,7 +3400,7 @@ class RtpReceiveStream(RtpReceiveCommon):
                     self.postMessage(f"{Fore.RED}ERR:__queueReceiverThread({self.syncSourceIdentifier}).get() Corrupted or non"
                                      f" existent rxQueue, {e}")
                     # Clear the reference to the rxQueue to stop it being polled any more
-                    self.rxQueue is None
+                    self.rxQueue = None
                     # Kill the stream
                     # self.killStream(caller=self)
             else:
